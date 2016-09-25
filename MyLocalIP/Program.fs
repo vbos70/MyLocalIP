a29f423378d86f681c7c79345cf2e0df109d294a
Victor Bos <victor.bos@gmail.com>
2016-09-25 22:03:55 +0300

    Check last saved IP address against current. Only update saved IP address if current IP address is different.

--- MyLocalIP/Program.fs
+++ MyLocalIP/Program.fs
@@ -1,12 +1,32 @@
 ﻿module Program
+open System.IO
 open System.Net
 open System.Net.Sockets
 
-let myLocalIP = (Dns.GetHostEntry (Dns.GetHostName ())).AddressList
-                |> Array.filter (fun (ip : IPAddress) -> ip.AddressFamily = AddressFamily.InterNetwork)
-                |> Array.head
+let fname = """C:\Users\vbos\Documents\SpiderOak Hive\lt141016-03-ip.txt"""
+let savedIP = try
+                File.ReadLines(fname) |> Seq.head
+              with
+                  | ex -> "No saved IP could be read"
+
+let myLocalIP = try
+                  ((Dns.GetHostEntry (Dns.GetHostName ())).AddressList
+                   |> Array.filter (fun (ip : IPAddress) -> ip.AddressFamily = AddressFamily.InterNetwork)
+                   |> Array.head).ToString()
+                with
+                    | ex -> "Unknown local IP"
 
 [<EntryPoint>]
 let main argv =
-    printfn "Local IP: %A" myLocalIP
+    //printfn "saved IP: %s" savedIP
+    //printfn "Local IP: %A" myLocalIP
+    if savedIP <> myLocalIP
+    then
+        try
+            let stream = new StreamWriter(fname, false)
+            stream.WriteLine(myLocalIP)
+            stream.Flush()
+            stream.Close()
+        with
+            | ex -> ()
     0 // Return an integer exit code
