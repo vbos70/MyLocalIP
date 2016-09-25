7122965bed6ae6e5a052b1801ae6626dc7290ddc
Victor Bos <victor.bos@gmail.com>
2016-09-25 21:07:38 +0300

    Print local IP address to stdout.

--- MyLocalIP/Program.fs
+++ MyLocalIP/Program.fs
@@ -1,7 +1,12 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module Program
+open System.Net
+open System.Net.Sockets
+
+let myLocalIP = (Dns.GetHostEntry (Dns.GetHostName ())).AddressList
+                |> Array.filter (fun (ip : IPAddress) -> ip.AddressFamily = AddressFamily.InterNetwork)
+                |> Array.head
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+    printfn "Local IP: %A" myLocalIP
+    0 // Return an integer exit code
